c19e1157e1c34b9789c10378ee89cbd77e75de8f
geno nullfree nullfree.geno@gmail.com <nullfree.geno@gmail.com>
2021-06-25 22:52:58 -0400

    Display file mode and permissions

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use clap::{App, AppSettings, Arg};
 use std::fs;
+use std::os::unix::fs::PermissionsExt;
 
 fn main() {
     let matches = App::new("lx")
@@ -18,7 +19,7 @@ fn main() {
     }
 }
 
-fn print_meta(file Vec<&str>) {
+fn print_meta(file: &str) {
     let meta = fs::metadata(file).unwrap();
-    println!("0x{:x}\t{}", meta.len(), i);
+    println!("{:06o}\t0x{:x}\t{}", meta.permissions().mode(), meta.len(), file);
 }
